827a76969bba83caaf6244b71758a46551ad83aa
Christopher <leechrs1@gmail.com>
2022-05-23 18:39:23 -0500

    GET routes don't work

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+//HTML routes. GET /notes should return notes.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+})
+
+//HTML routes. Get * should return index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+})
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`)
+})
