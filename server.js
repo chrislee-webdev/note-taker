0ff03badf8d50de96f8b15fe82e017debb4ee54e
Christopher <leechrs1@gmail.com>
2022-05-23 21:04:00 -0500

    can't get the notes to save

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
 //HTML routes. GET /notes should return notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
@@ -12,6 +16,18 @@ app.get('/notes', (req, res) => {
 //HTML routes. Get * should return index.html
 app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+//API routes should be created
+//GET /api/notes should read to db.json file and return all saved notes as JSON
+app.get('api/notes', (req, res) => {
+    res.json(notes);
+})
+//POST /api/notes should receive a new note to save on the request body and add it to the db.json file
+app.post('api/notes', (req, res) => {
+    req.body.id = notes.length.toString;
+    let newNote = createNote(req.body, notes);
+    res.json(newNote);
 })
 
 app.listen(3001, () => {
