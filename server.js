a451ee13c50ad02987e838e6afbb55b78c6eb4a1
Christopher <leechrs1@gmail.com>
2022-05-26 18:45:46 -0500

    can't get routes to work

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const app = express();
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const { notes } = require('data/notes');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+function createNewNote(body, notesArray) {
+    console.log(body);
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirnamr, './data/notes.json'),
+        JSON.stringify({ notes: notesArray}, null, 2)
+    );
+    return note;
+}
 
 //HTML routes. GET /notes should return notes.html
 app.get('/notes', (req, res) => {
@@ -20,14 +33,15 @@ app.get('/index', (req, res) => {
 
 //API routes should be created
 //GET /api/notes should read to db.json file and return all saved notes as JSON
-app.get('api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
+    let results = notes;
     res.json(notes);
 })
 //POST /api/notes should receive a new note to save on the request body and add it to the db.json file
-app.post('api/notes', (req, res) => {
-    req.body.id = notes.length.toString;
-    let newNote = createNote(req.body.id, notes);
-    res.json(newNote);
+app.post('/api/notes', (req, res) => {
+    req.body = notes.length.toString();
+    let note = createNewNote(req.body.id, notes);
+    res.json(note);
 })
 
 app.listen(3001, () => {
