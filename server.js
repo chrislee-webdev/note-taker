f885be9374c0d278880c1fc9f84b2957e873a9c8
Christopher <leechrs1@gmail.com>
2022-05-26 21:11:12 -0500

    still not working

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { notes } = require('data/notes');
+const { notes } = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -15,35 +17,32 @@ function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
     fs.writeFileSync(
-        path.join(__dirnamr, './data/notes.json'),
-        JSON.stringify({ notes: notesArray}, null, 2)
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
     );
     return note;
 }
 
-//HTML routes. GET /notes should return notes.html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
-
-//HTML routes. Get * should return index.html
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
-//API routes should be created
-//GET /api/notes should read to db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
     let results = notes;
-    res.json(notes);
-})
-//POST /api/notes should receive a new note to save on the request body and add it to the db.json file
-app.post('/api/notes', (req, res) => {
-    req.body = notes.length.toString();
-    let note = createNewNote(req.body.id, notes);
-    res.json(note);
+    res.json(results);
 })
 
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    //add note to json file and array
+    const note = createNewNote(req.body, notes)
+    res.json(note);
+});
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`)
-})
+    console.log(`API server now on port ${PORT}!`);
+});
