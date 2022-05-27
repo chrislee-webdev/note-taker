e69941968a58eead8c044525f52d2d6648022741
Christopher <leechrs1@gmail.com>
2022-05-26 22:03:31 -0500

    cleaned od formatting a little bit

--- server.js
+++ server.js
@@ -43,7 +43,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
