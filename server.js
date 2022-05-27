106d21da4d58f5659f44862832a3f59ef3480608
Christopher <leechrs1@gmail.com>
2022-05-26 21:36:05 -0500

    trying to publish to heroku

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+
 app.listen(3001, () => {
     console.log(`API server now on port ${PORT}!`);
 });
